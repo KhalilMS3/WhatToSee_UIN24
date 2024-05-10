153bd40118b1376aa57e42dacdf3ca447783a109
KhalilMS3 <kmsfouk@hiof.no>
2024-05-10 20:25:30 +0200

    created films schema in sanity (live coding)

---
+++ sanity/schemaTypes/films.js
@@ -0,0 +1,22 @@
+export const films = {
+   title: "Filmer",
+   name: "films",
+   type: "document",
+   fields: [
+      {
+         title: "Filmtittel",
+         name: "filmtitle",
+         type: "string",
+      },
+      {
+         title: "Filmposter",
+         name: "poster",
+         type: "image"
+      },
+      {
+         title: "imdbid",
+         name: "IMDBid",
+         type: "string"
+      }
+   ]
+}
--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -1,3 +1,4 @@
+import { films } from "./films";
 import { users } from "./users";
 
-export const schemaTypes = [users]
+export const schemaTypes = [users, films]
